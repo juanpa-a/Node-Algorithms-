8dfd39baed4c6acacc1f58f7cc412013026c2641
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2022-04-25 16:38:46 -0500

    stack

--- utlis/sanatize.js
+++ utlis/sanatize.js
@@ -5,3 +5,13 @@ str.replace(/[^a-z0-9]/gi, '')
 
 // remove white space
 str.replace(/\s/g, '')
+
+// count values in object
+const count = (val, store) => store[val] 
+    ? (++store[val]) 
+    : (store[val] = 1);
+
+// const store = {}
+// const count = (val) => store[val] 
+//     ? (++store[val]) 
+//     : (store[val] = 1);
